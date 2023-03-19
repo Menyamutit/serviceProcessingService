24e04c47bb2752080e33305f547f2b6b46d4214b
Menyamutit <115403386+Menyamutit@users.noreply.github.com>
2023-03-19 22:37:22 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+const express = require('express')
+
+const mongoose = require('mongoose')
+
+const router = require('./router.js')
+
+const PORT = 5000;
+const app = express()
+
+app.use(express.json())
+app.use("/api", router)
+
+
+
+
+
+
+async function starApp() {
+	try {
+		await mongoose.connect('mongodb+srv://dimagordienkog:123@cluster0.krqptta.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true })
+		app.listen(PORT, () => console.log("start server " + PORT))
+	} catch (e) {
+		res.status(500,).json(e)
+	}
+}
+
+
+
+starApp()
